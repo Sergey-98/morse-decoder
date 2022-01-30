426210a7cc8f7dffeacd8d9a6ebdfc3f2579cdcb
Sergey-98 <yatsenko1998@yandex.ru>
2022-01-30 22:32:36 +0400

    feat: complete task

--- src/index.js
+++ src/index.js
@@ -38,7 +38,35 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let SYM = {
+        '10': '.',
+        '11': '-',
+        '**': ' ',
+        '00': '',
+    }
+    let arr = [];
+    for (let j = 0; j < expr.length; j += 10) {
+        let s = expr.slice(j, j + 10);
+        arr.push(s);
+    }
+    arr = arr.map((elem) => {
+        let arr1 = [];
+        for (let i = 0; i < elem.length; i += 2) {
+            let m = elem.slice(i, i+2);
+            arr1.push(m);
+        }
+        return arr1;
+    });
+    arr = arr.map(comp => comp.map(el => SYM[el]))
+             .map(el => el.join(''))
+             .map(elem => MORSE_TABLE[elem]);
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] == undefined) {
+            arr[i] = ' ';
+        }
+    }
+    arr = arr.join('');
+    return arr;
 }
 
 module.exports = {
